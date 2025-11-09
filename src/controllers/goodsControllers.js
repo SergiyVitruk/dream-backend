58e86d2e854d4f9fd62823b30c0dabb61fc47806
SergiyVitruk <vitruk.sergiy@gmail.com>
2025-11-09 15:41:07 +0200

    fix filter

--- src/controllers/goodsControllers.js
+++ src/controllers/goodsControllers.js
@@ -2,8 +2,45 @@ import { Good } from '../models/good.js';
 import createHttpError from 'http-errors';
 
 export const getGoods = async (req, res) => {
-  const goods = await Good.find();
-  res.status(200).json(goods);
+  const {
+    page = 1,
+    perPage = 8,
+    category,
+    gender,
+    size,
+    priceMin,
+    priceMax,
+  } = req.query;
+  const skip = (page - 1) * perPage;
+
+  const goodsQuery = await Good.find();
+
+  if (category) {
+    goodsQuery.where('category').equals(category);
+  }
+
+  if (gender) {
+    goodsQuery.where('gender').equals(gender);
+  }
+
+  if (size) {
+    goodsQuery.where('size').equals(size);
+  }
+
+  const [totalItems, goods] = await Promise.all([
+    goodsQuery.clone().countDocuments(),
+    goodsQuery.skip(skip).limit(perPage),
+  ]);
+
+  const totalPages = Math.ceil(totalItems / perPage);
+
+  res.status(200).json({
+    page,
+    perPage,
+    totalItems,
+    totalPages,
+    goods,
+  });
 };
 
 export const getGoodsById = async (req, res, next) => {
--- src/routes/goodsRoutes.js
+++ src/routes/goodsRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { celebrate } from 'celebrate';
 import { getGoods, getGoodsById } from '../controllers/goodsControllers.js';
+import { getGoodsSchema, goodIdParamSchema } from '../validations/goodsValidation.js';
 
 const router = Router();
 
-router.get('/api/goods', getGoods);
-router.get('/api/goods/:goodId', getGoodsById);
+router.get('/api/goods', celebrate(getGoodsSchema), getGoods);
+router.get('/api/goods/:goodId', celebrate(goodIdParamSchema), getGoodsById);
 
 export default router;
