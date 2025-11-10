4e7867c116b179bd4b89c1145a0832263af002ef
liudmyla21-belikova <liudmyla.belikova@gmail.com>
2025-11-10 11:10:55 +0200

    create swagger-documentation

--- src/server.js
+++ src/server.js
@@ -3,6 +3,11 @@ import 'dotenv/config';
 import cors from 'cors';
 import helmet from 'helmet';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -14,9 +19,11 @@ import categoriesRoutes from './routes/categoriesRoutes.js';
 import feedbackRoutes from './routes/feedbackRoutes.js';
 import subscriptionRoutes from './routes/subscriptionRoutes.js';
 import ordersRoutes from './routes/ordersRoutes.js';
-
 import authRoutes from './routes/authRoutes.js';
-import cookieParser from 'cookie-parser';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml'));
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
@@ -27,6 +34,8 @@ app.use(cors());
 app.use(cookieParser());
 app.use(helmet());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(authRoutes);
 app.use(goodsRoutes);
 app.use(categoriesRoutes);
@@ -35,11 +44,11 @@ app.use(feedbackRoutes);
 app.use(subscriptionRoutes);
 
 app.use(errors());
-
 app.use(notFoundHandler);
 app.use(errorHandler);
 
 await connectMongoDB();
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`✅ Server is running on port ${PORT}`);
+  console.log(`📘 Swagger docs available at http://localhost:${PORT}/api-docs`);
 });
